b76fabd481b6c0c52cf0b0790863768acc224b1b
bnt10 <bnt10@naver.com>
2022-11-18 20:50:21 +0900

    Add terms checkBox in RegisterForm

---
+++ src/apis/terms.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosResponse } from 'axios'
+
+export type Terms = {
+  id: string
+  name: string
+  content: string
+  required: boolean
+  link: string
+}
+
+export interface GetTerms {
+  terms: Terms[]
+}
+export const signUpTerms = (): Promise<Terms[]> =>
+  axios.get<GetTerms>('/api/terms/sign-up').then((response) => response.data.terms)
